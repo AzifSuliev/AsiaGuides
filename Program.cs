55c21bdee876d477cf839b6cd033104f6c3bea22
azif suliev <azif.suliev.89@list.ru>
2025-06-01 15:40:45 +0500

    //

--- Program.cs
+++ Program.cs
@@ -50,7 +50,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapRazorPages();
 // Для проверки
-app.MapGet("/", () => "Hello from AsiaGuides!");
+//app.MapGet("/", () => "Hello from AsiaGuides!");
 
 app.MapControllerRoute(
     name: "root",
