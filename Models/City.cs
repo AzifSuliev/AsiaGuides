02afef9c59ba84958953c87482745b63d3d35577
azif suliev <azif.suliev.89@list.ru>
2025-06-07 10:58:45 +0500

    models 2

--- Models/City.cs
+++ Models/City.cs
@@ -8,14 +8,14 @@ namespace AsiaGuides.Models
         public int Id { get; set; }
         [Required]
         [MaxLength(30)]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         [Required]
         [MaxLength(100)]
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
         [Required(ErrorMessage = "You must select a country")]
         public int CountryId { get; set; }
-        public Country Country { get; set; }
+        public Country? Country { get; set; }
         public List<Attraction> Attractions { get; set; } = new();
     }
 }
